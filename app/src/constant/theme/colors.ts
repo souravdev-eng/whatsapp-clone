a8604256d30d456aebb6f77dba5b562165f7c02f
souravdev-eng <105838250+souravmusquaretech@users.noreply.github.com>
2022-08-20 10:08:15 +0530

    update color schem

--- app/src/constant/theme/colors.ts
+++ app/src/constant/theme/colors.ts
@@ -1,5 +1,9 @@
 export const colors = {
-  green: '#06E18D',
+  green: '#128C7E',
+  TealGreenDark: '#075E54',
+  LightGreen: '#25D366',
+  Blue: '#34B7F1',
+
   lightGreen: '#d3f6e0',
   black: '#1a1a1a',
   white: '#F8F9FE',
